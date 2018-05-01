a6fae8f075e100b101f5e94eb23a4049ec73915a
Daivik Goel <daivikgoel98@gmail.com>
2018-05-01 16:17:31 -0600

    Initial Commit

---
+++ Rock Paper Scissors/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rock Paper Scissors
+//
+//  Created by Daivik Goel on 2018-05-01.
+//  Copyright © 2018 Daivik Goel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
